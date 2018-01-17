7b355644b065ad276121407d1c1f376d5b45061f
lgants <lgants@gmail.com>
2018-01-16 22:15:54 -0600

    removed warning message

--- config/webpack.config.server.dev.js
+++ config/webpack.config.server.dev.js
@@ -10,9 +10,6 @@ module.exports = {
     path: paths.server.build
   },
   resolve: {
-    modules: ['node_modules', paths.nodeModules].concat(
-      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
-    ),
     extensions: ['.js'],
   },
   module: {
@@ -25,12 +22,14 @@ module.exports = {
           presets: [
             'react',
             'es2015'
-          ],
-          // This is a feature of `babel-loader` for webpack (not Babel itself).
-          // It enables caching results in ./node_modules/.cache/babel-loader/ directory for faster rebuilds.
-          cacheDirectory: true,
+          ]
         }
       }
     ],
-  }
+  },
+  // watchOptions: {
+  //   aggregateTimeout: 300,
+  //   poll: 500,
+  //   ignored: /node_modules/
+  // }
 };
